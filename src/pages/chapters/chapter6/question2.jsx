6192deb9b77b1c4edc40efe96e4a83943e28bc68
Tony Boutros <tonyboutros@Tonys-MacBook-Pro.local>
2024-01-08 10:09:31 +0200

    ewew

--- src/pages/chapters/chapter6/question2.jsx
+++ src/pages/chapters/chapter6/question2.jsx
@@ -29,9 +29,11 @@ const Question2 = () => {
         enText={ <>Labour <CircleDot enText="Lebanese Labour Law of 1946: Applicable to all workers except domestic and agricultural workers Ministry of Labour sets guidelines on working conditions and conducts labour inspections" arText="قانون العمل اللبناني لعام 1946 ينطبق على جميع العمال وأصحاب العمل باستثناء العمال المنزليين، والعمال الزراعيين، والمؤسسات المحدودة بأعضاء الأسرة فقط، والموظفين العموميين.
         تحدد وزارة العمل الإرشادات لتنظيم التوظيف وظروف العمل لجميع الموظفين وهي مسؤولة عن إجراء تفتيش العمل.
         " color="#c0000f" /></> }
-        arText="العمل"
+        
+        arText={ <>العمل<CircleDot enText="Lebanese Labour Law of 1946: Applicable to all workers except domestic and agricultural workers Ministry of Labour sets guidelines on working conditions and conducts labour inspections" arText="قانون العمل اللبناني لعام 1946 ينطبق على جميع العمال وأصحاب العمل باستثناء العمال المنزليين، والعمال الزراعيين، والمؤسسات المحدودة بأعضاء الأسرة فقط، والموظفين العموميين.
+        تحدد وزارة العمل الإرشادات لتنظيم التوظيف وظروف العمل لجميع الموظفين وهي مسؤولة عن إجراء تفتيش العمل." color="#c0000f" /></> }
        />
-
+   
    
        <Paragraph
         enText="Lebanese Labour Law of 1946 : Applicable to all workers except domestic and agricultural workers Ministry of Labour sets guidelines on working conditions and conducts labour inspections"
@@ -44,7 +46,13 @@ const Question2 = () => {
         enText="
         The legal basis for the Lebanese EIA system is established in the Environment Protection Law No. 444/2002 and the EIA decree No. 8633/2012 and its annexes. The EIA decree and its annexes include all the requirements for screening, preparation of the environmental assessment and the supervision of the environmental assessment process. As per Lebanon’s Environmental Protection Law possible environmental risks should be assessed and mitigation measures and opportunities for sustainable solutions developed."
          /> </>}
-        arText="قانون حماية البيئة"
+   
+        arText={<>قانون حماية البيئة
+          <CircleDot 
+          color="blue"
+          enText="The legal basis for the Lebanese EIA system is established in the Environment Protection Law No. 444/2002 and the EIA decree No. 8633/2012 and its annexes. The EIA decree and its annexes include all the requirements for screening, preparation of the environmental assessment and the supervision of the environmental assessment process. As per Lebanon’s Environmental Protection Law possible environmental risks should be assessed and mitigation measures and opportunities for sustainable solutions developed."
+          arText="الأساس القانوني لنظام تقييم الأثر البيئي اللبناني محدد في قانون حماية البيئة رقم 444/2002 ومرسوم تقييم الأثر البيئي رقم 8633/2012 وملحقاته. ويتضمن مرسوم تقييم الأثر البيئي وملحقاته كافة متطلبات التقييم وإعداد التقييم البيئي والإشراف على عملية التقييم البيئي. وفقًا لقانون حماية البيئة اللبناني، ينبغي تقييم المخاطر البيئية المحتملة ووضع تدابير التخفيف وفرص الحلول المستدامة."
+          /> </>}
        />
        <Paragraph
         enText="Environmental Protection Law No.444/20022 EIA decree No. 8633/2012 and annexes Annex 1 list for which an EIA study is required for construction Annex 2 list for which an Initial Environmental Examination (IEE) is required for construction Annex 3 list of projects located in sensitive areas"
